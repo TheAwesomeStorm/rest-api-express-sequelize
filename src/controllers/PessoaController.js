096bc7cf56cead80aef01e6888ac40884eb50296
Daniel Mendes Sechinske <dms@tokenizadora.com.br>
2022-11-16 18:18:01 -0300

    added endpoints to manage Matriculas

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -68,6 +68,68 @@ class PessoaController {
       return res.status(500).json(e.message);
     }
   }
+
+  static async readMatriculaByIdFromPessoaIdAsync(req, res) {
+    const { estudanteId, matriculaId } = req.params;
+    try {
+      const matricula = await models.Matriculas.findOne({
+        where: {
+          id: Number(matriculaId),
+          estudante_id: Number(estudanteId),
+        }
+      });
+      return res.status(200).json(matricula);
+    } catch (e) {
+      return res.status(500).json(e.message);
+    }
+  }
+
+  static async createMatriculaFromPessoaIdAsync(req, res) {
+    const { estudanteId } = req.params;
+    const matricula = { ...req.body, estudante_id: Number(estudanteId) };
+    try {
+      const createdMatricula = await models.Matriculas.create(matricula);
+      return res.status(200).json(createdMatricula);
+    } catch (e) {
+      return res.status(500).json(e.message);
+    }
+  }
+
+  static async updateMatriculaFromPessoaIdAsync(req, res) {
+    const { estudanteId, matriculaId } = req.params;
+    const matricula = req.body;
+    try {
+      await models.Matriculas.update(matricula, {
+        where: {
+          id: Number(matriculaId),
+          estudante_id: Number(estudanteId),
+        }
+      });
+      const updatedMatricula = await models.Matriculas.findOne({
+        where: {
+          id: Number(matriculaId),
+        }
+      })
+      return res.status(200).json(updatedMatricula);
+    } catch (e) {
+      return res.status(500).json(e.message);
+    }
+  }
+
+  static async deleteMatriculaFromPessoaIdAsync(req, res) {
+    const { matriculaId, estudanteId } = req.params;
+    try {
+      await models.Matriculas.destroy({
+        where: {
+          id: Number(matriculaId),
+          estudante_id: Number(estudanteId),
+        }
+      });
+      return res.status(200).json({ message: `Matrícula de ID ${matriculaId}, do estudante de ID ${estudanteId}, removida com sucesso.` });
+    } catch (e) {
+      return res.status(500).json(e.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
--- src/routes/pessoaRoutes.js
+++ src/routes/pessoaRoutes.js
@@ -8,6 +8,10 @@ router
   .get('/pessoas/:id', PessoaController.readPessoaByIdAsync)
   .post('/pessoas', PessoaController.createPessoaAsync)
   .put('/pessoas/:id', PessoaController.updatePessoaAsync)
-  .delete('/pessoas/:id', PessoaController.deletePessoaAsync);
+  .delete('/pessoas/:id', PessoaController.deletePessoaAsync)
+  .get('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.readMatriculaByIdFromPessoaIdAsync)
+  .post('/pessoas/:estudanteId/matriculas', PessoaController.createMatriculaFromPessoaIdAsync)
+  .put('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.updateMatriculaFromPessoaIdAsync)
+  .delete('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.deleteMatriculaFromPessoaIdAsync);
 
 module.exports = router;
